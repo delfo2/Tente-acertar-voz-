ad3356541e606b6d2112e6cd8f73afcac3f600c5
Carlos Eduardo <105376328+delfo2@users.noreply.github.com>
2022-12-07 16:16:34 -0300

    colocando chances limitadas e fazendo tela de derrota

--- js/validaNumero.js
+++ js/validaNumero.js
@@ -1,45 +1,71 @@
 const pagina = document.querySelector('[data-corpo]');
 
+let chances = 7;
+
 function validaNumero (chute) {
-    const number = +chute;
-    
-    if (numeroIsTrue(number)) {
-        grupoElemento.innerHTML += '<h3 class="corpo__erro">Chute inválido: O chute precisa ser um número.</h3>';
-        return;
-    }
 
-    if (numeroIsBetween(number)) {
-        grupoElemento.innerHTML += `<h3 class="corpo__erro">Chute inválido: O número precisa estar entre ${maiorNumero} e ${menorNumero}.</h3>`;
-        return;
-    }
-
-    if (number === numeroSecreto) {
-        pagina.innerHTML = `
-        <h1 class="corpo__titulo">
+    if(chances >= 0) {
+        const number = +chute;
+        
+        if (numeroIsTrue(number)) {
+            grupoElemento.innerHTML += '<h3 class="corpo__erro">Chute inválido: O chute precisa ser um número.</h3>';
+            return;
+        }
+        
+        if (numeroIsBetween(number)) {
+            grupoElemento.innerHTML += `<h3 class="corpo__erro">Chute inválido: O número precisa estar entre ${maiorNumero} e ${menorNumero}.</h3>`;
+            return;
+        }
+        
+        if (number === numeroSecreto) {
+            pagina.innerHTML = `
+            <h1 class="corpo__titulo">
             <i class="fa-solid fa-comment"></i>
-            Tente Acertar o Número
+                Tente Acertar o Número
             <i class="fa-solid fa-microphone"></i>
-        </h1>
-        <h2 class="corpo__acerto">Você acertou!</h2>
-        <h3 class="corpo__dica">O número secreto era: ${numeroSecreto}.</h3>
-        <button class="corpo__botao" id="btn-jogar_novamente">Jogar novamente</button>
-        `;
-    } else if (number > numeroSecreto) {
-        grupoElemento.innerHTML += `
+            </h1>
+            <h2 class="corpo__acerto">Você acertou!</h2>
+            <h3 class="corpo__dica">O número secreto era: ${numeroSecreto}.</h3>
+            <button class="corpo__botao" id="btn-jogar_novamente">Jogar novamente</button>
+            `;
+        } else if (number > numeroSecreto) {
+            chances -= 1;
+            grupoElemento.innerHTML += `
             <span>
                 <i class="fa-solid fa-circle-arrow-down"></i>
                     O número secreto é menor que isso!!
                 <i class="fa-solid fa-circle-arrow-down"></i>
+                <br>
+                tentativas restantes: ${chances}
             </span>
-        `
-    } else {
-        grupoElemento.innerHTML += `
-        <span>
-            <i class="fa-solid fa-circle-arrow-up"></i>
-                O número secreto é maior que isso!!
-            <i class="fa-solid fa-circle-arrow-up"></i>
-        </span>
-    `
+            `
+        } else {
+            chances -= 1;
+            grupoElemento.innerHTML += `
+            <span>
+                <i class="fa-solid fa-circle-arrow-up"></i>
+                    O número secreto é maior que isso!!
+                <i class="fa-solid fa-circle-arrow-up"></i>
+                <br>
+                tentativas restantes: ${chances}
+            </span>
+            `
+        }
+        if(chances === 0) {
+            podeJogar = false;
+            recognition.stop();
+            pagina.innerHTML = `
+                <h1 class="corpo__titulo">
+                    <i class="fa-solid fa-comment"></i>
+                        Tente Acertar o Número
+                    <i class="fa-solid fa-microphone"></i>
+                </h1>
+                <h2 class="corpo__acerto">Você perdeu!</h2>
+                <h3 class="corpo__dica">Suas dicas acabaram</h3>
+                <h3 class="corpo__dica">O número secreto era: ${numeroSecreto}.</h3>
+                <button class="corpo__botao" id="btn-jogar_novamente">Jogar novamente</button>
+            `;
+        }
     }
 }
 
